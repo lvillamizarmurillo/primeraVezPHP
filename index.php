17aee4e700a777a698eea9ebafffe90766a26a1e
lvillamizarmurillo <lvillamizarmurillo@gmail.com>
2023-05-29 17:15:54 -0500

    feat: :sparkles: Punto 8
    Punto 8 realizado referente a los tipos de datos php

--- index.php
+++ index.php
@@ -8,20 +8,36 @@
     // //sprintf
     // $mensaje = sprintf("\n%s capos del php",$texto);
     // echo $mensaje;
-    //punto 7 variables y constantes
-    //numérica
-    $numero = 26;
-    //texto
-    $nombre = "Ludwing";
+    // //punto 7 variables y constantes
+    // //numérica
+    // $numero = 26;
+    // //texto
+    // $nombre = "Ludwing";
+    // //booleano
+    // $es_valido = true;
+    // //var_dump imprime el tipo de dato y contenido de la variable
+    // echo var_dump($nombre);
+    // //constantes
+    // //numérica
+    // define("PI", 3.14);
+    // //texto
+    // define("SALUDO","Hola compañeros del yutu");
+    // //booleano
+    // define("ES_INCORRECTO",false);
+    //Punto 8 Tipos de datos
     //booleano
-    $es_valido = true;
-    //var_dump imprime el tipo de dato y contenido de la variable
-    echo var_dump($nombre);
-    //constantes
-    //numérica
-    define("PI", 3.14);
-    //texto
-    define("SALUDO","Hola compañeros del yutu");
-    //booleano
-    define("ES_INCORRECTO",false);
+    $logueado = true;
+    var_dump($logueado);
+    //Enteros
+    $numero = 10;
+    var_dump($numero);
+    //Floats
+    $flotante = 12.3;
+    var_dump($flotante);
+    //Strings
+    $cadenaTexto = "Jorge";
+    var_dump($cadenaTexto);
+    //array
+    $array = [];
+    var_dump($array);
 ?>
