f481b4271b673d90fcc35b0ee13d0490a4a142d5
lvillamizarmurillo <lvillamizarmurillo@gmail.com>
2023-05-29 17:02:49 -0500

    feat: :sparkles: punto 6
    Se termino el punto 6 con ejemplos

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
-    $nombre = (string) "Ludwing";
-    echo $nombre;
+    //Punto 6, maneras de imprimir un texto con echo, printf y sprintf
+    //echo
+    echo "Texto a imprimir";
+    //printf
+    $texto = "Hola";
+    printf("\n%s Mundo",$texto);
+    //sprintf
+    $mensaje = sprintf("\n%s capos del php",$texto);
+    echo $mensaje;
 ?>
