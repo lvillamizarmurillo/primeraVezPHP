c0cbec03469ddc84e351a298eda1e37f2261a95f
lvillamizarmurillo <lvillamizarmurillo@gmail.com>
2023-05-29 17:09:42 -0500

    feat: :sparkles: Punto 7
    Se realizo todos los ejemplos del punto 7 explicando la sintaxis

--- index.php
+++ index.php
@@ -1,11 +1,27 @@
 <?php
-    //Punto 6, maneras de imprimir un texto con echo, printf y sprintf
-    //echo
-    echo "Texto a imprimir";
-    //printf
-    $texto = "Hola";
-    printf("\n%s Mundo",$texto);
-    //sprintf
-    $mensaje = sprintf("\n%s capos del php",$texto);
-    echo $mensaje;
+    // //Punto 6, maneras de imprimir un texto con echo, printf y sprintf
+    // //echo
+    // echo "Texto a imprimir";
+    // //printf
+    // $texto = "Hola";
+    // printf("\n%s Mundo",$texto);
+    // //sprintf
+    // $mensaje = sprintf("\n%s capos del php",$texto);
+    // echo $mensaje;
+    //punto 7 variables y constantes
+    //numérica
+    $numero = 26;
+    //texto
+    $nombre = "Ludwing";
+    //booleano
+    $es_valido = true;
+    //var_dump imprime el tipo de dato y contenido de la variable
+    echo var_dump($nombre);
+    //constantes
+    //numérica
+    define("PI", 3.14);
+    //texto
+    define("SALUDO","Hola compañeros del yutu");
+    //booleano
+    define("ES_INCORRECTO",false);
 ?>
