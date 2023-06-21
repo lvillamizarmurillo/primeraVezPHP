6765ce78c4cf75420d3c0c19dbf11deb0d4c7b12
lvillamizarmurillo <lvillamizarmurillo@gmail.com>
2023-06-21 12:28:16 -0500

    feat: :sparkles: POO php, herencia
    Se deja ejemplo de herencioa con php.

--- index.php
+++ index.php
@@ -1,33 +1,63 @@
 <?php
     //PHP INTERMEDIO
-    //Programación orientada a objetos
-    //clases
-    class Capibara{
-        private $nombre;
-        protected $edad;
+    // //Programación orientada a objetos
+    // //clases
+    // class Capibara{
+    //     private $nombre;
+    //     protected $edad;
 
-        public function __construct($nombre,$edad){
-            $this->nombre = $nombre;
-            $this->edad = $edad;
+    //     public function __construct($nombre,$edad){
+    //         $this->nombre = $nombre;
+    //         $this->edad = $edad;
+    //     }
+    //     public function getNombre(){
+    //         return $this->nombre;
+    //     }
+    //     public function setNombre($nombre){
+    //         $this->nombre = $nombre;
+    //     }
+    //     public function getEdad(){
+    //         return $this->edad;
+    //     }
+    //     public function setEdad($nombre){
+    //         $this->edad = $edad;
+    //     }
+    //     public function saludar(){
+    //         echo "Hola, mi nombre es ".$this->nombre." y mi edad es: ".$this->edad;
+    //     }
+    // }
+    // $alumno = new Capibara("Chancho",25);
+    // $alumno->getNombre();//Se omite con el constructor
+    // $alumno->getEdad();//Se omite con el constructor
+    // $alumno->saludar();
+    //Herencia
+    class Transporte{
+        public function __construct(protected int $ruedas, protected int $capacidad){
+
         }
-        public function getNombre(){
-            return $this->nombre;
+        public function getInfo() : string{
+            return "El transporte tiene ".$this->ruedas." ruedas y una capacidad de ".$this->capacidad." personas.";
         }
-        public function setNombre($nombre){
-            $this->nombre = $nombre;
-        }
-        public function getEdad(){
-            return $this->edad;
-        }
-        public function setEdad($nombre){
-            $this->edad = $edad;
-        }
-        public function saludar(){
-            echo "Hola, mi nombre es ".$this->nombre." y mi edad es: ".$this->edad;
+        public function getRuedas() : int {
+            return $this->ruedas;
         }
     }
-    $alumno = new Capibara("Chancho",25);
-    $alumno->getNombre();//Se omite con el constructor
-    $alumno->getEdad();//Se omite con el constructor
-    $alumno->saludar();
+    class Bicicleta extends Transporte{
+        public function getInfo() : string{
+            return "El transporte tiene ".$this->ruedas." ruedas y una capacidad de ".$this->capacidad." personas Y NO GASTA GASOLINA.";
+        }
+    }
+    class Automovil extends Transporte{
+        public function __construct(protected int $ruedas, protected int $capacidad, protected string $transmision){
+
+        }
+        public function getTransmision(){
+            return $this->transmision;
+        }
+    }
+    $mercedes = new Automovil(4,5,"Manual");
+    echo $mercedes->getInfo();
+    echo $mercedes->getTransmision();
+    $bmx = new Bicicleta(2,1);
+    echo $bmx->getInfo();
 ?>
