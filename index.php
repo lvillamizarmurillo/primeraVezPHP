2d5712617a05fa2142f141ee85030393d5a57471
lvillamizarmurillo <lvillamizarmurillo@gmail.com>
2023-06-22 11:52:36 -0500

    feat: :sparkles: POO, polimorfismo
    Se evidencia un ejemplo de como usar polimorfismo.

--- index.php
+++ index.php
@@ -84,23 +84,51 @@
     // $tripaSeca = new Camello();
     // $chompiras->hacerSonido();
     // $tripaSeca->hacerSonido();
-    //Interfaces
-    interface Figura{
-        public function calcularArea();
+    // //Interfaces
+    // interface Figura{
+    //     public function calcularArea();
+    // }
+    // interface Figura3D extends Figura{
+    //     public function calcularVolumen();
+    // }
+    // class Cubo implements Figura3D{
+    //     public function __construct(private int $lado){}
+    //     public function calcularArea(){
+    //         return 6*pow($this->lado,2);
+    //     }
+    //     public function calcularVolumen(){
+    //         return pow($this->lado,3);
+    //     }
+    // }
+    // $cubo = new Cubo(5);
+    // echo $cubo->calcularArea();
+    // echo $cubo->calcularVolumen();
+    //Polimorfismo
+    interface TransporteInterfaz {
+        public function getInfo() : string;
+        public function getRuedas() : int;
     }
-    interface Figura3D extends Figura{
-        public function calcularVolumen();
-    }
-    class Cubo implements Figura3D{
-        public function __construct(private int $lado){}
-        public function calcularArea(){
-            return 6*pow($this->lado,2);
+    class Transporte implements TransporteInterfaz{
+        public function __construct(protected int $ruedas,protected int $capacidad){}
+        public function getInfo() : string {
+            return "El transporte cuenta con ".$this->ruedas." ruedas y tiene una capacidad de ".$this->capacidad." personas.";
         }
-        public function calcularVolumen(){
-            return pow($this->lado,3);
+        public function getRuedas() : int {
+            return $this->ruedas;
         }
     }
-    $cubo = new Cubo(5);
-    echo $cubo->calcularArea();
-    echo $cubo->calcularVolumen();
+    class Automovil extends Transporte implements TransporteInterfaz{
+        public function __construct(protected int $ruedas,protected int $capacidad,protected string $color){}
+        public function getInfo(): string{
+            return "El auto cuenta con ".$this->ruedas." ruedas y tiene una capacidad de ".$this->capacidad." personas. Aparte es de color ".$this->color;
+        }
+        public function getColor(){
+            return "El color es: ".$this->color;
+        }
+    }
+    $transporte = new Transporte(8,20);
+    $bmw = new Automovil(4,5,"Negro");
+    echo $transporte->getInfo();
+    echo $bmw->getInfo();
+    echo $bmw->getColor();
 ?>
