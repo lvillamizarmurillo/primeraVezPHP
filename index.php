61689a78c2b4a598bc2ccc76574deb6f93a8b812
lvillamizarmurillo <lvillamizarmurillo@gmail.com>
2023-06-22 12:07:29 -0500

    feat: :sparkles: POO, autoload
    Un ejemplo de como se instancia una clase desde otro archivo con autoload

--- index.php
+++ index.php
@@ -103,32 +103,39 @@
     // $cubo = new Cubo(5);
     // echo $cubo->calcularArea();
     // echo $cubo->calcularVolumen();
-    //Polimorfismo
-    interface TransporteInterfaz {
-        public function getInfo() : string;
-        public function getRuedas() : int;
+    // //Polimorfismo
+    // interface TransporteInterfaz {
+    //     public function getInfo() : string;
+    //     public function getRuedas() : int;
+    // }
+    // class Transporte implements TransporteInterfaz{
+    //     public function __construct(protected int $ruedas,protected int $capacidad){}
+    //     public function getInfo() : string {
+    //         return "El transporte cuenta con ".$this->ruedas." ruedas y tiene una capacidad de ".$this->capacidad." personas.";
+    //     }
+    //     public function getRuedas() : int {
+    //         return $this->ruedas;
+    //     }
+    // }
+    // class Automovil extends Transporte implements TransporteInterfaz{
+    //     public function __construct(protected int $ruedas,protected int $capacidad,protected string $color){}
+    //     public function getInfo(): string{
+    //         return "El auto cuenta con ".$this->ruedas." ruedas y tiene una capacidad de ".$this->capacidad." personas. Aparte es de color ".$this->color;
+    //     }
+    //     public function getColor(){
+    //         return "El color es: ".$this->color;
+    //     }
+    // }
+    // $transporte = new Transporte(8,20);
+    // $bmw = new Automovil(4,5,"Negro");
+    // echo $transporte->getInfo();
+    // echo $bmw->getInfo();
+    // echo $bmw->getColor();
+    //Autoload
+    function my_autoload($clase){
+        require __DIR__.'/clases/'.$clase.'.php';
     }
-    class Transporte implements TransporteInterfaz{
-        public function __construct(protected int $ruedas,protected int $capacidad){}
-        public function getInfo() : string {
-            return "El transporte cuenta con ".$this->ruedas." ruedas y tiene una capacidad de ".$this->capacidad." personas.";
-        }
-        public function getRuedas() : int {
-            return $this->ruedas;
-        }
-    }
-    class Automovil extends Transporte implements TransporteInterfaz{
-        public function __construct(protected int $ruedas,protected int $capacidad,protected string $color){}
-        public function getInfo(): string{
-            return "El auto cuenta con ".$this->ruedas." ruedas y tiene una capacidad de ".$this->capacidad." personas. Aparte es de color ".$this->color;
-        }
-        public function getColor(){
-            return "El color es: ".$this->color;
-        }
-    }
-    $transporte = new Transporte(8,20);
-    $bmw = new Automovil(4,5,"Negro");
-    echo $transporte->getInfo();
-    echo $bmw->getInfo();
-    echo $bmw->getColor();
+    spl_autoload_register('my_autoload');
+    $detalles = new Detalles();
+    $clientes = new Clientes();
 ?>
